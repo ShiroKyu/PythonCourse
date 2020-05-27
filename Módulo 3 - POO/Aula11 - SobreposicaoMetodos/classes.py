46da7423ec9f728cf2936a410e2731873f6a5c3a
Paulo <paulo.leite@academico.ifpb.edu.br>
2020-05-26 22:23:09 -0300

    Herança multipla

---
+++ Módulo 3 - POO/Aula11 - SobreposicaoMetodos/classes.py
@@ -0,0 +1,32 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        self.nomeclasse = self.__class__.__name__
+
+    def falar(self):
+        print(f'{self.nomeclasse} falando')
+
+
+class Cliente(Pessoa):
+    def comprar(self):
+        print(f'{self.nomeclasse} comprando')
+
+
+class ClienteVIP(Cliente):
+    def __init__(self, nome, idade, val_assinatura):
+        # Chama o construtor de Pessoa
+        super().__init__(nome, idade)
+        self.val_assinatura = val_assinatura
+
+    # Sobrescrevendo o método da classe pai
+    def falar(self):
+        # executa primeiro o método da classe pai e depois o da filha
+        # super().falar()
+
+        # executa o método de uma classe especifica
+        Pessoa.falar(self)
+        print('kk eae men')
+
+    def listar(self):
+        print(f'{self.nome}/{self.idade}/{self.val_assinatura}')
---
+++ Módulo 3 - POO/Aula11 - SobreposicaoMetodos/main.py
@@ -0,0 +1,7 @@
+from classes import *
+
+
+c1 = ClienteVIP('Maria', 48, 49.90)
+c1.falar()
+c1.comprar()
+c1.listar()
